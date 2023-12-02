9a16d3d1cb07a6ff65d4e853dd8261813ef358cc
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-12-02 19:50:36 +0600

    tripple.js

--- tripple.js
+++ tripple.js
@@ -1,5 +1,13 @@
-const first = 2;
-const second = '2';
+// const first = 2;
+// const second = '2';
+// if(first === second){
+//     console.log('Values are equal');
+// }
+// else{
+//     console.log('Values are not equal');
+// }
+const first = [];
+const second = first;
 if(first === second){
     console.log('Values are equal');
 }
