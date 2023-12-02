a2dccf63e6a8b21cafceffac0a1a61c0e4778df9
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-12-02 18:09:57 +0600

    undefine.js

--- undefine.js
+++ undefine.js
@@ -2,3 +2,26 @@
 let first ;
 console.log(first);
 // 2.
+function second(a,b){
+    const total = a+b;
+
+}
+
+const result = second();
+console.log(result);
+// 3.
+function result1(a,b,c){
+    const total = a+b+c;
+    console.log(a,b,c);
+}
+const add = result1(2,2);
+console.log(add)
+// 4.
+function noNegative(a,b){
+    if( a < 0 || b<0){
+        return
+    }
+    return a+b
+}
+const total = noNegative(-5,4);
+console.log(total);
