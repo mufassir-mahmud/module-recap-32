80e646fa591c9869d6ebcd04d0739be54c8fad1f
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-12-02 22:17:32 +0600

    Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,8 @@
+for(let i = 0; i<5; i++){
+    console.log(i);
+}
+console.log(i);
+// for(var i = 0; i<5; i++){
+//     console.log(i);
+// }
+// console.log(i);
---
+++ scope.js
@@ -0,0 +1,11 @@
+function add(a,b){
+    const total = a+b;
+    console.log(a,b);
+    if(b>5){
+        const sum  = 20 + a +b;
+        console.log(sum);
+    }
+    return total
+}
+// console.log(a,b);
+add(5,4)
